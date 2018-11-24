1bfd1ac11c809816c28d258fdb714c540ee72099
natan33 <srassis2014@gmail.com>
2018-11-23 22:19:38 -0200

    Nova alteração

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -15,7 +15,7 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("projeto 3");
+        System.out.println("testar");
     }
     
 }
